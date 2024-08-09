04c7d20be2615fb0e38b8e2167d6f3c52fdbf26a
JavaliMZ <syjulio123@gmail.com>
2024-08-09 09:20:42 +0100

    Committing changes

--- getTLSInformationToTable/__init__.py
+++ getTLSInformationToTable/__init__.py
@@ -1,19 +1,28 @@
 import ssl
 import sys
 import socket
+import json
+import subprocess
 from pprint import pprint
 from datetime import datetime
 from urllib.parse import urlparse
 
+
+# Basic Command:
+# tlsx -u https://www.google.com -ex -ss -mm -re -un -ve -ce -ct all -o /tmp/tlsx_output.csv
 
 def try_except(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except Exception as e:
+            print(f"Error when run this function: {func.__name__}")
             print(f"Error: {e}")
     return wrapper
 
+def read_json_file(file):
+    content = open(file, "r").read()
+    return json.loads(content)
 
 @try_except
 def get_tls_validity(url):
@@ -32,17 +41,22 @@ def get_tls_validity(url):
     }
     return ssl_info_dict
 
-
+@try_except
+def get_system_command(command):
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
+    return output, error
 
 def main():
-    url = sys.argv[1]
-    pprint(f"Checking TLS information for {url}")
-    ssl_info = get_tls_validity(url)
-    if ssl_info:
-        pprint(ssl_info)
-    else:
-        pprint("No TLS information found")
+    list_of_urls_file = sys.argv[1]
+    command = f"tlsx -L {list_of_urls_file} -ex -ss -mm -re -un -ve -ce -ct all -o /tmp/{list_of_urls_file}.result.tlsx.output"
+    output, error = get_system_command(command)
+    pprint(output)
+    pprint(error)
+
 
 
 if __name__ == "__main__":
     main()
+
+# main()
